f50702ef250aa62a580fc4dbf76f99aada457b31
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-05-31 17:09:04 +0100

    fix(config): revert to vitest configuration in place of jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+/// <reference types="vitest/config" />
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5174,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3300',
+        changeOrigin: true,
+        secure: false // dev mode
+      }
+    },
+    cors: {
+      // the origin you will be accessing via browser
+      origin: 'http://localhost:3300'
+    }
+  },
+  test: {
+    name: { label: 'unit', color: 'cyan' },
+    globals: true, // use describe, it, expect without import
+    environment: 'jsdom', // for React (DOM environment)
+    setupFiles: './jest.setup.ts' // optional setup file,
+    // testTimeout: 15000
+  }
+});
